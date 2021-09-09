1dce12ca68bf9bae537ad2f5cc7a43771668b66a
Kimbeomjin <advensod@daum.net>
2021-09-09 22:53:52 +0900

    day4

--- day4/ex5.js
+++ day4/ex5.js
@@ -3,14 +3,17 @@ let end;
 
 function startNo() {
     start = prompt('시작위치');
+    console.log(start);
 }
 function endNo() {
     end = prompt('끝나는 위치');
+    console.log(end);
 }
 
 function strMethod() {
-    let input
-    input = prompt('문자열을 입력하세요');
+    let input = prompt('문자열을 입력하세요');
+    console.log(start);
+    console.log(end);
     document.getElementById("start").innerHTML = start;
     document.getElementById("end").innerHTML = end;
     document.getElementById("len").innerHTML = input.length;
