dcd4a83624268f61de6ce7657b180bbaa0e8bf97
Kimbeomjin <advensod@daum.net>
2021-09-13 09:28:04 +0900

    day4

--- day4/ex5.js
+++ day4/ex5.js
@@ -1,23 +1,18 @@
 let start;
-let end;
+      let end;
 
-function startNo() {
-    start = prompt('시작위치');
-    console.log(start);
-}
-function endNo() {
-    end = prompt('끝나는 위치');
-    console.log(end);
-}
-
-function strMethod() {
-    let input = prompt('문자열을 입력하세요');
-    console.log(start);
-    console.log(end);
-    document.getElementById("start").innerHTML = start;
-    document.getElementById("end").innerHTML = end;
-    document.getElementById("len").innerHTML = input.length;
-    document.getElementById("last").innerHTML = input.lastIndexOf("am");
-    document.getElementById("subs").innerHTML = input.substring(start, end);
-    document.getElementById("slice").innerHTML = input.substring(end);
-}
+      function startNo() {
+        start = prompt("시작인덱스");
+      }
+      function endNo() {
+        end = prompt("시작인덱스");
+      }
+      function strMethod() {
+        let input = prompt("문자열을 입력하세요");
+        document.getElementById("start").innerHTML = start;
+        document.getElementById("end").innerHTML = end;
+        document.getElementById("len").innerHTML = input.length;
+        document.getElementById("last").innerHTML = input.lastIndexOf("am");
+        document.getElementById("subs").innerHTML = input.substring(start, end);
+        document.getElementById("slice").innerHTML = input.slice(end);
+      }
